38eebb338e7e704392c2610a6131d1ff609d7a80
Ekaluk52003 <ekaluk.p@gmail.com>
2022-03-12 14:35:00 +0700

    with vm

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ import { isProd } from './utils';
 
 const main = async () => {
   dotenv.config();
-  console.log('watch git pre-production')
+  console.log('with posgres volumn')
   const RedisClient = new Redis(process.env.REDIS_URL);
 //   const RedisClient = new Redis({
 //     host:  'srv-captain--redis-caprover',
